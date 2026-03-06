62bd6e1383ca9bc27c275394d00512166d6d83d3
DarthGomes <lucas.sp_13@hotmail.com>
2026-03-05 23:43:10 -0300

    feat: :sparkles: main orchestrator created

---
+++ src/main.py
@@ -0,0 +1,42 @@
+from src.utils.logger import get_logger
+
+from src.ingestion import run_ingestion
+
+from src.transformations.store_products_transformation import run_store_products
+from src.transformations.store_sales_order_detail import run_store_sales_order_detail
+from src.transformations.store_sales_order_header import run_store_sales_order_header
+
+from src.transformations.publish_products import run_publish_products
+from src.transformations.publish_orders import run_publish_orders
+
+logger = get_logger(__name__)
+
+
+def main():
+
+    logger.info("Starting Sales Analytics Pipeline")
+
+    try:
+
+        # RAW
+        run_ingestion()
+
+        # STORE
+        run_store_products()
+        run_store_sales_order_detail()
+        run_store_sales_order_header()
+
+        # PUBLISH
+        run_publish_products()
+        run_publish_orders()
+
+        logger.info("Pipeline finished successfully")
+
+    except Exception as e:
+
+        logger.exception("Pipeline failed")
+        raise e
+
+
+if __name__ == "__main__":
+    main()
--- src/transformations/store_sales_order_detail.py
+++ src/transformations/store_sales_order_detail.py
@@ -9,22 +9,29 @@ from src.utils.table_writer import save_table
 spark = get_spark()
 logger = get_logger(__name__)
 
-df_sales_order_detail = spark.table('raw.raw_sales_order_detail')
-
-def build_store_sales_order_detail(df_sales_order_detail: DataFrame) -> DataFrame:
+def build_store_sales_order_detail(df: DataFrame) -> DataFrame:
 
     df_casted = (
-        df_sales_order_detail
+        df
         .withColumn("UnitPrice", F.col("UnitPrice").cast(DecimalType(18,2)))
         .withColumn("UnitPriceDiscount", F.col("UnitPriceDiscount").cast(DecimalType(5,4)))
     )
 
     return df_casted
 
-df_sales_order_detail_cleaned = df_sales_order_detail.transform(build_store_sales_order_detail)
 
-save_table(
-    df_sales_order_detail_cleaned,
-    schema="store",
-    table_name="store_sales_order_detail"
-)
+def run_store_sales_order_detail():
+
+    logger.info("Starting store_sales_order_detail transformation")
+
+    df = spark.table("raw.raw_sales_order_detail")
+
+    df_cleaned = df.transform(build_store_sales_order_detail)
+
+    save_table(
+        df_cleaned,
+        schema="store",
+        table_name="store_sales_order_detail"
+    )
+
+    logger.info("store_sales_order_detail saved successfully")
