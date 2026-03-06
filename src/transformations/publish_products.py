ab4e9472a9ea77be32de22f903ed7fbb71b30cc3
DarthGomes <lucas.sp_13@hotmail.com>
2026-03-05 23:04:16 -0300

    fix :bug: found out an opportunity to fullfill empty categorys by creating new ones from subcategory

--- src/transformations/publish_products.py
+++ src/transformations/publish_products.py
@@ -10,7 +10,28 @@ logger = get_logger(__name__)
 
 products_df = spark.table("store.store_products")
 
+
 def create_publish_products(df: DataFrame) -> DataFrame:
+
+    logger.info("Starting publish_products transformation")
+
+    clothing = [
+        "Gloves", "Shorts", "Socks", "Tights", "Vests",
+        "Caps", "Bib-Shorts"
+    ]
+
+    accessories = [
+        "Locks", "Lights", "Headsets", "Helmets", "Pedals", "Pumps",
+        "Hydration Packs", "Bottles and Cages", "Panniers",
+        "Bike Racks", "Fenders", "Bike Stands", "Cleaners"
+    ]
+
+    components = [
+        "Wheels", "Saddles",
+        "Bottom Brackets", "Derailleurs", "Chains",
+        "Forks", "Brakes", "Cranksets", "Tires and Tubes"
+    ]
+
     publish_products = (
         df
         .withColumn(
@@ -22,23 +43,19 @@ def create_publish_products(df: DataFrame) -> DataFrame:
             "ProductCategoryName",
             F.when(
                 (F.col("ProductCategoryName").isNull()) &
-                (F.col("ProductSubCategoryName").isin(
-                    "Gloves", "Shorts", "Socks", "Tights", "Vests"
-                )),
+                (F.col("ProductSubCategoryName").isin(clothing)),
                 F.lit("Clothing")
             )
             .when(
                 (F.col("ProductCategoryName").isNull()) &
-                (F.col("ProductSubCategoryName").isin(
-                    "Locks", "Lights", "Headsets", "Helmets", "Pedals", "Pumps"
-                )),
+                (F.col("ProductSubCategoryName").isin(accessories)),
                 F.lit("Accessories")
             )
             .when(
                 (F.col("ProductCategoryName").isNull()) &
                 (
                     F.col("ProductSubCategoryName").contains("Frames") |
-                    F.col("ProductSubCategoryName").isin("Wheels", "Saddles")
+                    F.col("ProductSubCategoryName").isin(components)
                 ),
                 F.lit("Components")
             )
@@ -46,12 +63,19 @@ def create_publish_products(df: DataFrame) -> DataFrame:
         )
     )
 
+    logger.info("publish_products transformation finished")
+
     return publish_products
 
+
 df_publish_products = products_df.transform(create_publish_products)
+
+logger.info("Saving publish_products table")
 
 save_table(
     df=df_publish_products,
     schema="publish",
     table_name="publish_products"
 )
+
+logger.info("publish_products saved successfully")
